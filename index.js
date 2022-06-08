2c8f77462b8d098378fd0c44cfb11ab8a51df06d
codigocaballer <josemariacaballerm@gmail.com>
2022-06-09 00:37:09 +0200

    disable es 6

--- index.js
+++ index.js
@@ -3,21 +3,11 @@
 
 // init project
 var express = require('express');
-//import express from 'express';
-//import {getDatePart, getTimePart} from "./public/common.mjs";
-
-import path from 'path';
-import {fileURLToPath} from "url";
-
 var app = express();
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
-import cors from 'cors';
-
+var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
