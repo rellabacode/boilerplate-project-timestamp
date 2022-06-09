5af4fd19357c8fba25fa28350689e66be0b4ed3e
codigocaballer <josemariacaballerm@gmail.com>
2022-06-09 04:16:19 +0200

    improve impl

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ var app = express();
 
 
 // Your own super cool function
-var logger = function(req, res, next) {
-    console.log("GOT REQUEST ! "+ req.url);
-    next(); // Passing the request to the next handler in the stack.
-}
+// var logger = function(req, res, next) {
+//     console.log("GOT REQUEST ! "+ req.url);
+//     next(); // Passing the request to the next handler in the stack.
+// }
 
-app.use(logger); // Here you add your logger to the stack.
+// app.use(logger); // Here you add your logger to the stack.
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
@@ -27,21 +27,38 @@ app.get("/", function (req, res) {
     res.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/api/:year(19[7-9][0-9]|[2-9][0-9][0-9][0-9])-?:month(0[1-9]|1[0-2])-?:day(0[1-9]|1[0-9]|2[0-9]|3[0-1])" +
-    "(T:hour(0[0-9]|1[0-9]|2[0-3])\::minute(0[0-9]|1[0-9]|2[0-9]|3[0-9]|4[0-9]|5[0-9])\::second(0[0-9]|1[0-9]|2[0-9]|3[0-9]|4[0-9]|5[0-9])(\.[0-9][0-9][0-9]))?", function (req, res) {
-    let milliseconds = (req.params.hour ?
-        Date.UTC(req.params.year, req.params.month - 1, req.params.day, req.params.hour, req.params.minute, req.params.second) :
-        Date.UTC(req.params.year, req.params.month - 1, req.params.day));
-
-    let date = new Date(milliseconds);
-
-    res.json({unix: date.getTime(), utc: date.toUTCString()});
-});
-
 app.get("/api/:unix(\\d{1,})", function (req, res) {
-    let date = new Date(parseInt(req.params.unix));
-    res.json({unix: date.getTime(), utc: date.toUTCString()});
+    console.log(req.params);
+    var date = (req.params.unix ? new Date(parseInt(req.params.unix)) : new Date());
+
+    if (date instanceof Date && !isNaN(date.valueOf())) {
+        res.json({unix: date.getTime(), utc: date.toUTCString()});
+    }
+    return res.json({error: "Invalid Date"});
 });
+
+app.get("(/api|/api/(:year(19[7-9][0-9]|[2-9][0-9][0-9][0-9])-:month(0[1-9]|1[0-2])-:day(0[1-9]|1[0-9]|2[0-9]|3[0-1])" +
+    "(T:hour(0[0-9]|1[0-9]|2[0-3])\::minute(0[0-9]|1[0-9]|2[0-9]|3[0-9]|4[0-9]|5[0-9])\::second(0[0-9]|1[0-9]|2[0-9]|3[0-9]|4[0-9]|5[0-9])" +
+    "(\.[0-9]{3}[+-]:zhour(0[0-9]|1[0-9]|2[0-4])::zmin(0[0-9]|1[0-9]|2[0-9]|3[0-9]|4[0-9]|5[0-9]))?)?)?)?|/api/:dateString",
+    function (req, res) {
+
+        console.log(req.params);
+
+        if (req.params.year === undefined && req.params.hour === undefined) {
+            let current = new Date();
+            return res.json({unix: current.getTime(), utc: current.toUTCString()});
+        }
+
+        let milliseconds = (req.params.hour ?
+            Date.UTC(req.params.year, req.params.month - 1, req.params.day, req.params.hour, req.params.minute, req.params.second) :
+            Date.UTC(req.params.year, req.params.month - 1, req.params.day));
+
+        let date = new Date(milliseconds);
+        if (date instanceof Date && !isNaN(date.valueOf())) {
+            res.json({unix: date.getTime(), utc: date.toUTCString()});
+        }
+        return res.json({error: "Invalid Date"});
+    });
 
 // listen for requests :)
 //process.env.PORT
